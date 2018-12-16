929666e714571a2962a7129aed5f69aa2cd4f903
Richard Goulter <richard.goulter@MX15-pro>
2018-12-16 15:50:58 +0700

    refactor to use a global hashtable rather than a global value

--- buffer-ribbon.el
+++ buffer-ribbon.el
@@ -130,8 +130,10 @@ past its defined"
 
 ;;;; patch grid methods
 
-(defvar buffer-ribbon/global-patch-grid nil
-  "use buffer-ribbon/current-patch-grid function instead of accessing this directly")
+(defvar buffer-ribbon/global-patch-grid-hash (make-hash-table)
+  "Use buffer-ribbon/current-patch-grid function instead of accessing this directly.
+
+The hash table with the hash table of patch grids, keyed by name.")
 
 (defun buffer-ribbon/set-patch-grid-window-parameters (window patch-grid-position)
   (set-window-parameter window 'is-patch-grid t)
@@ -142,6 +144,14 @@ past its defined"
 
 (defun buffer-ribbon/patch-grid-window-position (window)
   (window-parameter window 'patch-grid-position))
+
+(defun buffer-ribbon/window-patch-grid-name (window)
+  "The name of the patch grid that the window is associated with."
+  (window-parameter window 'patch-grid-name))
+
+(defun buffer-ribbon/set-window-patch-grid-name (window name)
+  "Sets the name of the patch grid that the window is associated with."
+  (set-window-parameter window 'patch-grid-name name))
 
 (defun buffer-ribbon/make-patch-grid (buffer-ribbon windows)
   (let ((patch-grid (list 'patch-grid
@@ -200,8 +210,40 @@ past its defined"
                     (list new-windows)))
       (signal 'wrong-type-argument (list buffer-ribbon/patch-grid-p patch-grid))))
 
+(defun buffer-ribbon/patch-grid (name)
+  (gethash name buffer-ribbon/global-patch-grid-hash))
+
+(defun buffer-ribbon/window-patch-grid (window)
+  (let ((patch-grid-name (buffer-ribbon/window-patch-grid-name window)))
+    (buffer-ribbon/patch-grid patch-grid-name)))
+
 (defun buffer-ribbon/current-patch-grid ()
-  buffer-ribbon/global-patch-grid)
+  (buffer-ribbon/window-patch-grid (selected-window)))
+
+(defun buffer-ribbon/next-unique-key (hash key &optional number)
+  "Finds a next key which will be unique for the hash table.
+
+key must be a string."
+  (let ((key-candidate (if number (format "%s<%d>" key number) key)))
+    (if (not (gethash key-candidate hash))
+      key-candidate
+      (buffer-ribbon/next-unique-key
+       hash
+       key
+       (if number (+ 1 number) 2)))))
+
+(defun buffer-ribbon/next-patch-grid-name ()
+  (buffer-ribbon/next-unique-key
+   buffer-ribbon/global-patch-grid-hash
+   "patch-grid"))
+
+(defun buffer-ribbon/register-patch-grid (patch-grid)
+  (let ((name (buffer-ribbon/next-patch-grid-name))
+        (windows (buffer-ribbon/patch-grid-windows patch-grid)))
+    (puthash name patch-grid buffer-ribbon/global-patch-grid-hash)
+    (mapcar (lambda (window)
+              (buffer-ribbon/set-window-patch-grid-name window name))
+            windows)))
 
 (defun buffer-ribbon/patch-grid-width (&optional patch-grid)
   "number of columns the patch grid has"
@@ -307,7 +349,7 @@ selected window."
          (patch-grid (buffer-ribbon/make-patch-grid
                       buffer-ribbon
                       grid-windows)))
-    (setq buffer-ribbon/global-patch-grid patch-grid)
+    (buffer-ribbon/register-patch-grid patch-grid)
     (buffer-ribbon/update-buffer-ribbon-from-patch-grid buffer-ribbon patch-grid)))
 
 (defun buffer-ribbon/init-from-current-windows ()
@@ -323,7 +365,7 @@ frame will be considered for the patch grid."
          (patch-grid (buffer-ribbon/make-patch-grid
                       buffer-ribbon
                       current-windows)))
-    (setq buffer-ribbon/global-patch-grid patch-grid)
+    (buffer-ribbon/register-patch-grid patch-grid)
     (buffer-ribbon/update-buffer-ribbon-from-patch-grid buffer-ribbon patch-grid)))
 
 (defun buffer-ribbon/scroll-buffer-ribbon-left ()
