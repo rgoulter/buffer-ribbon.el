2c7ed33aa48b9b7fd3db83a49bb74b79f54e5ccb
Richard Goulter <richard.goulter@MX15-pro>
2018-12-16 15:50:58 +0700

    refactor to use a global hashtable rather than a global value

--- buffer-ribbon-tests.el
+++ buffer-ribbon-tests.el
@@ -16,18 +16,6 @@
   (should-not (equal nil
                      (buffer-ribbon/make-buffer-ribbon))))
 
-(defun buffer-ribbon/kludge-use-blank-global-vars (body)
-  "kludge to allow test which uses the ribbon/patch grid
-which is currently a global variable."
-  (let ((old-patch-grid buffer-ribbon/global-patch-grid)
-        (old-buffer-ribbon (buffer-ribbon/current-buffer-ribbon)))
-  (unwind-protect
-      (progn (setq buffer-ribbon/global-ribbon nil)
-             (setq buffer-ribbon/global-patch-grid nil)
-             (funcall body))
-    (progn (setq buffer-ribbon/global-ribbon old-buffer-ribbon)
-           (setq buffer-ribbon/global-patch-grid old-patch-grid)))))
-
 ;; My experience: Debian on WSL can't run make-frame in -batch,
 ;; (make-frame also doesn't work in MSYS2 shell, or CMD shell;
 ;;  although set-frame-size doesn't work in emacs -batch run from
@@ -51,79 +39,75 @@ which is currently a global variable."
         (set-window-configuration old-window-config)))))
 
 (ert-deftest buffer-ribbon/test-e2e-from-existing-scroll-right-scroll-left ()
-  (buffer-ribbon/kludge-use-blank-global-vars
-   (lambda ()
-     ;; ASSEMBLE
-     ;;; make a frame
-     (buffer-ribbon/run-with-test-frame
-       (lambda (test-frame)
-         ;;; split into 3x2
-         (buffer-ribbon/split-into-3-2)
-         ;;; set each of them to distinct buffers
-         (dotimes (i 6)
-           (set-window-buffer nil (get-buffer-create (int-to-string (+ 1 i))))
-           (other-window +1))
-         ;;; now init from existing grid
-         (buffer-ribbon/init-from-current-windows)
-         (let ((old-patch-grid-buffers
-                (mapcar 'window-buffer
-                        (buffer-ribbon/patch-grid-windows (buffer-ribbon/current-patch-grid)))))
-           ;; ACT
-           ;;; shift to the right
-           (buffer-ribbon/scroll-buffer-ribbon-right)
-           ;;; set the two 'new' grid buffers
-           ;;; (since we shifted 'right' & started in (0,0),
-           ;;;  need to replace (0,0) and (0,1)):
-           (set-window-buffer nil (get-buffer-create "7"))
-           (other-window +1)
-           (set-window-buffer nil (get-buffer-create "8"))
-           (other-window -1)
-           ;;; shift back to the left
-           (buffer-ribbon/scroll-buffer-ribbon-left)
-           ;; ASSERT
-           ;;; check that the buffers are the same as we set them
-           (let ((actual-patch-grid-buffers (mapcar 'window-buffer
-                        (buffer-ribbon/patch-grid-windows (buffer-ribbon/current-patch-grid)))))
-           (should (equal old-patch-grid-buffers
-                          actual-patch-grid-buffers)))))))))
+  ;; ASSEMBLE
+  ;;; make a frame
+  (buffer-ribbon/run-with-test-frame
+    (lambda (test-frame)
+      ;;; split into 3x2
+      (buffer-ribbon/split-into-3-2)
+      ;;; set each of them to distinct buffers
+      (dotimes (i 6)
+        (set-window-buffer nil (get-buffer-create (int-to-string (+ 1 i))))
+        (other-window +1))
+      ;;; now init from existing grid
+      (buffer-ribbon/init-from-current-windows)
+      (let ((old-patch-grid-buffers
+             (mapcar 'window-buffer
+                     (buffer-ribbon/patch-grid-windows (buffer-ribbon/current-patch-grid)))))
+        ;; ACT
+        ;;; shift to the right
+        (buffer-ribbon/scroll-buffer-ribbon-right)
+        ;;; set the two 'new' grid buffers
+        ;;; (since we shifted 'right' & started in (0,0),
+        ;;;  need to replace (0,0) and (0,1)):
+        (set-window-buffer nil (get-buffer-create "7"))
+        (other-window +1)
+        (set-window-buffer nil (get-buffer-create "8"))
+        (other-window -1)
+        ;;; shift back to the left
+        (buffer-ribbon/scroll-buffer-ribbon-left)
+        ;; ASSERT
+        ;;; check that the buffers are the same as we set them
+        (let ((actual-patch-grid-buffers (mapcar 'window-buffer
+                     (buffer-ribbon/patch-grid-windows (buffer-ribbon/current-patch-grid)))))
+        (should (equal old-patch-grid-buffers
+                       actual-patch-grid-buffers)))))))
 
 (ert-deftest buffer-ribbon/test-e2e-from-existing-scroll-left-scroll-right ()
-  (buffer-ribbon/kludge-use-blank-global-vars
-   (lambda ()
-     ;; ASSEMBLE
-     ;;; make a frame
-     (buffer-ribbon/run-with-test-frame
-       (lambda (test-frame)
-         ;;; split into 3x2
-         (buffer-ribbon/split-into-3-2)
-         ;;; set each of them to distinct buffers
-         (dotimes (i 6)
-           (set-window-buffer nil (get-buffer-create (int-to-string (+ 1 i))))
-           (other-window +1))
-         ;;; now init from existing grid
-         (buffer-ribbon/init-from-current-windows)
-         (let ((old-patch-grid-buffers
-                (mapcar 'window-buffer
-                        (buffer-ribbon/patch-grid-windows (buffer-ribbon/current-patch-grid)))))
-           ;; ACT
-           ;;; shift to the left
-           (buffer-ribbon/scroll-buffer-ribbon-left)
-           ;;; set the two 'new' grid buffers
-           ;;; (since we shifted 'left' & started in (0,0),
-           ;;;  need to replace (2,0) and (2,1)):
-           (other-window +4)
-           (set-window-buffer nil (get-buffer-create "7"))
-           (other-window +1)
-           (set-window-buffer nil (get-buffer-create "8"))
-           (other-window -5)
-           ;;; shift back to the right
-           (buffer-ribbon/scroll-buffer-ribbon-right)
-           ;; ASSERT
-           ;;; check that the buffers are the same as we set them
-           (let ((actual-patch-grid-buffers (mapcar 'window-buffer
-                        (buffer-ribbon/patch-grid-windows (buffer-ribbon/current-patch-grid)))))
-           (should (equal old-patch-grid-buffers
-                          actual-patch-grid-buffers)))))))))
+  ;; ASSEMBLE
+  ;;; make a frame
+  (buffer-ribbon/run-with-test-frame
+    (lambda (test-frame)
+      ;;; split into 3x2
+      (buffer-ribbon/split-into-3-2)
+      ;;; set each of them to distinct buffers
+      (dotimes (i 6)
+        (set-window-buffer nil (get-buffer-create (int-to-string (+ 1 i))))
+        (other-window +1))
+      ;;; now init from existing grid
+      (buffer-ribbon/init-from-current-windows)
+      (let ((old-patch-grid-buffers
+             (mapcar 'window-buffer
+                     (buffer-ribbon/patch-grid-windows (buffer-ribbon/current-patch-grid)))))
+        ;; ACT
+        ;;; shift to the left
+        (buffer-ribbon/scroll-buffer-ribbon-left)
+        ;;; set the two 'new' grid buffers
+        ;;; (since we shifted 'left' & started in (0,0),
+        ;;;  need to replace (2,0) and (2,1)):
+        (other-window +4)
+        (set-window-buffer nil (get-buffer-create "7"))
+        (other-window +1)
+        (set-window-buffer nil (get-buffer-create "8"))
+        (other-window -5)
+        ;;; shift back to the right
+        (buffer-ribbon/scroll-buffer-ribbon-right)
+        ;; ASSERT
+        ;;; check that the buffers are the same as we set them
+        (let ((actual-patch-grid-buffers (mapcar 'window-buffer
+                     (buffer-ribbon/patch-grid-windows (buffer-ribbon/current-patch-grid)))))
+        (should (equal old-patch-grid-buffers
+                       actual-patch-grid-buffers)))))))
 
 (provide 'buffer-ribbon-tests)
 
