e5e7daf9ee8c2ab9286446b88df15410495cb357
Richard Goulter <richard.goulter@gmail.com>
2018-12-21 12:35:18 +0700

    rearrange code to conform to ert-runner.el conventions

--- run-tests.sh
+++ run-tests.sh
@@ -3,5 +3,6 @@ emacs -batch \
       -l ert \
       -l dev/bootstrap.el \
       -l buffer-ribbon.el \
-      -l tests/buffer-ribbon-tests.el \
+      -l test/test-helper.el \
+      -l test/buffer-ribbon-test.el \
       -f ert-run-tests-batch-and-exit
